e2eaf3618102effa7c7aba27bb7f428746f1192b
Adam Zaninovich <adam.zaninovich@gmail.com>
2012-02-14 13:55:08 -0800

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbtodo::Application.config.secret_token = '7ee262e8de075daacdd1090352fd192bb575680e854ca013aa6ba083cf82d86409d5da997f11197d685c391e775136421aae579f4971cc43510d26da55a0eb0a'
